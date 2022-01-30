eea83b7cc4e8453d2d690396e904eebeadf21df9
Samuel Jones <ssjones424@gmail.com>
2022-01-29 21:25:34 -0500

    Initial untested HAL implementations to catch HAL up with simulator

--- src/hw/cryptography_hal_esp32c.c
+++ src/hw/cryptography_hal_esp32c.c
@@ -4,18 +4,33 @@
 
 #include <stdint.h>
 #include <stddef.h>
+#include <string.h>
 #include "cryptography_hal.h"
 #include "mbedtls/rsa.h"
 #include "mbedtls/pem.h"
+#include "mbedtls/sha256.h"
+#include "mbedtls/pk.h"
+#include "esp_random.h"
+#include "freertos/FreeRTOS.h"
+#include "filesystem_hal.h"
 
 void cryptography_init(void) {
+}
 
+int _cryptography_fill_random(void* ctx, unsigned char *data, size_t len) {
+    esp_fill_random(data, len);
+    return (int)len;
 }
 
 
 bool cryptography_rsa_exists(const char* private_filename,
                              const char* public_filename,
                              const char* uuid_filename) {
+    struct stat s;
+    // TODO do proper validation and backup/restore
+    if (0 == FS_Stat(private_filename, &s) && 0 == FS_Stat(public_filename, &s)) {
+        return true;
+    }
     return false;
 }
 
@@ -23,7 +38,91 @@ bool cryptography_rsa_exists(const char* private_filename,
 bool cryptography_rsa_generate(const char *private_filename,
                                const char *public_filename,
                                const char *uuid_filename) {
-    return false;
+
+    FS_Remove(uuid_filename);
+    file_handle f = FS_Open(uuid_filename, "w");
+    if (!f) {
+        printf("Failed to open key UUID file\n");
+        return false;
+    }
+
+    unsigned char bytes[16];
+    uint8_t *b = bytes;
+    esp_fill_random(bytes, 16);
+
+    char uuid[100];
+    int len = snprintf(uuid, 100, "uuid = \"%02x%02x%02x%02x-%02x%02x-%02x%02x-%02x%02x-%02x%02x%02x%02x%02x%02x\"\n",
+                       b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7], b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15]);
+    FS_Write(f, uuid, len);
+    FS_Close(f);
+
+    mbedtls_pk_context pk_ctx = {0};
+    mbedtls_pk_init(&pk_ctx);
+    mbedtls_pk_setup( &pk_ctx, mbedtls_pk_info_from_type(MBEDTLS_PK_RSA));
+    mbedtls_rsa_context *rsa = mbedtls_pk_rsa(pk_ctx);
+    //mbedtls_rsa_init(rsa, MBEDTLS_RSA_PKCS_V21, MBEDTLS_MD_SHA256);
+
+    int result = mbedtls_rsa_gen_key(rsa, _cryptography_fill_random, NULL, 2048, 0x10001);
+    if (result != 0) {
+        mbedtls_pk_free(&pk_ctx);
+        printf("RSA Generation error: %d", result);
+        return false;
+    }
+    result = mbedtls_rsa_check_pub_priv(rsa, rsa);
+    if (result != 0) {
+        mbedtls_pk_free(&pk_ctx);
+        printf("RSA validation error: %d", result);
+    }
+
+    unsigned char *key_data = pvPortMalloc(5000);
+
+    FS_Remove(private_filename);
+    f = FS_Open(private_filename, "w");
+    if (!f) {
+        printf("Failed to open private key file\n");
+        mbedtls_pk_free(&pk_ctx);
+        vPortFree(key_data);
+        return false;
+    }
+
+    result = mbedtls_pk_write_key_pem(&pk_ctx, key_data, 5000);
+    if (result != 0) {
+        printf("Error exporting private key\n");
+        FS_Close(f);
+        mbedtls_pk_free(&pk_ctx);
+        vPortFree(key_data);
+        return false;
+    }
+
+    // TODO check file write errors;
+    FS_Write(f, key_data, (int)strlen((char*)key_data));
+    FS_Close(f);
+
+    FS_Remove(public_filename);
+    f = FS_Open(public_filename, "w");
+    if (!f) {
+        printf("Failed to open public key file\n");
+        mbedtls_pk_free(&pk_ctx);
+        vPortFree(key_data);
+        return false;
+    }
+
+    result = mbedtls_pk_write_pubkey_pem(&pk_ctx, key_data, 5000);
+    if (result != 0) {
+        printf("Error exporting public key\n");
+        FS_Close(f);
+        mbedtls_pk_free(&pk_ctx);
+        vPortFree(key_data);
+        return false;
+    }
+
+    FS_Write(f, key_data, (int)strlen((char*)key_data));
+    FS_Close(f);
+
+    vPortFree(key_data);
+    mbedtls_pk_free(&pk_ctx);
+
+    return true;
 }
 
 
@@ -31,7 +130,18 @@ bool cryptography_digest_sha(const uint8_t *data,
                              size_t len,
                              size_t bits,
                              uint8_t *output) {
-    return false;
+
+    if (bits != 256) {
+        printf("Unsupported SHA mode\n");
+        return false;
+    }
+    int result = mbedtls_sha256_ret(data, len, output, false);
+
+    if (result != 0) {
+        printf("SHA hash calculation failure! (%d)", result);
+        return false;
+    }
+    return true;
 }
 
 bool cryptography_sign_rsa(const char* private_filename,
@@ -39,5 +149,35 @@ bool cryptography_sign_rsa(const char* private_filename,
                            size_t len,
                            uint8_t **output,
                            size_t *output_len) {
-    return false;
+
+    mbedtls_pk_context pk_ctx = {0};
+    mbedtls_pk_init(&pk_ctx);
+    mbedtls_pk_setup( &pk_ctx, mbedtls_pk_info_from_type(MBEDTLS_PK_RSA));
+
+    struct stat fstat;
+    FS_Stat(private_filename, &fstat);
+    file_handle f = FS_Open(private_filename, "r");
+    if (!f) {
+        printf("No PEM file for signing!\n");
+        mbedtls_pk_free(&pk_ctx);
+        return false;
+    }
+
+    int result;
+    result = mbedtls_pk_parse_keyfile(&pk_ctx, private_filename, NULL);
+    if (result != 0) {
+        printf("Failed to import data from PEM file! (%d)\n", result);
+        return false;
+    }
+
+    *output = pvPortMalloc(256);
+
+    result = mbedtls_pk_sign(&pk_ctx, MBEDTLS_MD_NONE, data, len, *output, output_len, _cryptography_fill_random, NULL);
+    mbedtls_pk_free(&pk_ctx);
+    if (result != 0) {
+        printf("Failed to generate signature! (%d)\n", result);
+        vPortFree(output);
+    }
+
+    return true;
 }
--- src/hw/wifi_hal_esp32c.c
+++ src/hw/wifi_hal_esp32c.c
@@ -9,6 +9,9 @@
 #include "esp_event.h"
 #include "esp_wifi.h"
 #include "esp_http_client.h"
+#include "filesystem_hal.h"
+
+#include "sntp.h"
 
 static EventGroupHandle_t _event_group;
 static TimerHandle_t _timeout_timer;
@@ -19,6 +22,7 @@ typedef enum {
     ESP_WIFI_STARTED = (1<<2),
     ESP_WIFI_STOPPED = (1<<3),
     ESP_WIFI_TIMEOUT = (1<<4),
+    ESP_WIFI_TIME_SYNC = (1<<5),
 } ESP_WIFI_BITS;
 
 static void start_timeout_timer(uint32_t ms) {
@@ -80,6 +84,7 @@ void WIFI_Init(void) {
 
     xEventGroupWaitBits(_event_group, ESP_WIFI_STARTED, true, false, portMAX_DELAY);
 
+    sntp_init();
 }
 
 bool WIFI_Connect(const char* ssid, const char* password) {
@@ -116,6 +121,8 @@ bool WIFI_Connect(const char* ssid, const char* password) {
         return false;
     }
 
+    sntp_init();
+
     return true;
 }
 
@@ -140,17 +147,99 @@ bool WIFI_HttpGet(const char* host,
                   const char* response_filename,
                   int *status) {
 
-    esp_err_t err;
+    //TODO Implement saving headers - not done for now
 
-    esp_http_client_config_t config = {
+    esp_http_client_config_t config = {0};
+    esp_http_client_handle_t handle = esp_http_client_init(&config);
 
-    };
-    //err = esp_http_client_init()
+    size_t full_path_len = strlen(host) + strlen(subdirectory) + 1;
+    char* path = pvPortMalloc(full_path_len);
+    strcpy(path, host);
+    strcat(path, subdirectory);
+    esp_http_client_set_url(handle, path);
+    vPortFree(path);
 
-    return false;
+
+    for (int i=0; i<header_count; i++) {
+        size_t header_len = strlen(headers[i]);
+        char* header_copy = pvPortMalloc(header_len+1);
+        strcpy(header_copy, headers[i]);
+        char* kv_break = strrchr(header_copy, ':');
+        *kv_break = 0;
+        printf("Setting header, key (%s) value (%s)", header_copy, kv_break+2);
+        esp_http_client_set_header(handle, header_copy, kv_break+2);
+
+        vPortFree(header_copy);
+    }
+
+    esp_http_client_set_method(handle, HTTP_METHOD_GET);
+    esp_http_client_open(handle, 0);
+    int content_length = esp_http_client_fetch_headers(handle);
+    if (content_length < 0) {
+        printf("HTTP client fetch headers failed\n");
+        esp_http_client_close(handle);
+        esp_http_client_cleanup(handle);
+        return false;
+    }
+    *status = esp_http_client_get_status_code(handle);
+
+    file_handle f = FS_Open(response_filename, "wb");
+    if (!f) {
+        printf("Couldn't open response file!\n");
+        esp_http_client_close(handle);
+        esp_http_client_cleanup(handle);
+        return false;
+    }
+
+    while (1) {
+        char buffer[128];
+        if (esp_http_client_is_complete_data_received(handle)) {
+            break;
+        }
+        int data_read = esp_http_client_read_response(handle, buffer, 128);
+        if (data_read >= 0) {
+            FS_Write(f, buffer, data_read);
+        }
+    }
+    FS_Close(f);
+    esp_http_client_close(handle);
+    esp_http_client_cleanup(handle);
+
+    return true;
+
 }
 
+struct timeval _time;
+void _time_sync_notification(struct timeval *tv) {
+    memcpy(&_time, tv, sizeof(struct timeval));
+    xEventGroupSetBits(_event_group,ESP_WIFI_TIME_SYNC);
+}
+
+
+char *cur_ntp_server;
 uint32_t WIFI_GetNetworkTime(const char* host) {
 
-    return 0;
+    if (cur_ntp_server) {
+        vPortFree(cur_ntp_server);
+    }
+
+    size_t len = strlen(host);
+    cur_ntp_server = pvPortMalloc(len+1);
+    strcpy(cur_ntp_server, host);
+
+    sntp_setoperatingmode(SNTP_OPMODE_POLL);
+    sntp_setservername(0, cur_ntp_server);
+    sntp_set_time_sync_notification_cb(_time_sync_notification);
+    sntp_init();
+
+    printf("Waiting for time sync event");
+
+    xEventGroupClearBits(_event_group, ESP_WIFI_TIME_SYNC);
+    EventBits_t bits = xEventGroupWaitBits(_event_group, ESP_WIFI_TIME_SYNC,
+                                           true, false, portMAX_DELAY);
+    
+    printf("Unix time: %u", _time.tv_sec);
+
+    sntp_stop();
+    return _time.tv_sec;
 }
